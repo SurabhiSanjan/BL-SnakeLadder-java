68effb49e4f858e2ec317a95af6b7f734e4b086f
SurabhiSanjan <surabhisanjan@gmail.com>
2022-10-20 13:10:57 +0530

    Addition: getting the Die Roll active

--- src/com/BridgelabzSLD/SnakeLadder.java
+++ src/com/BridgelabzSLD/SnakeLadder.java
@@ -4,6 +4,8 @@ public class SnakeLadder {
     public static void main(String args[]){
         System.out.println("This is a Single player Snake & Ladder Game");
         int pos;
-        
+        Random r=new Random();
+        int dieRoll=r.nextInt((5) +1)+1;
+        System.out.println("You Got :" + dieRoll);
     }
 }
