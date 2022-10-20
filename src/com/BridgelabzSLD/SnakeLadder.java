4ce5bdbd3e88a39c6d3ed3001f997abf5c79e4d5
SurabhiSanjan <surabhisanjan@gmail.com>
2022-10-20 13:03:05 +0530

    Initiating Snake Ladder Game

---
+++ src/com/BridgelabzSLD/SnakeLadder.java
@@ -0,0 +1,9 @@
+package com.BridgelabzSLD;
+import java.util.*;
+public class SnakeLadder {
+    public static void main(String args[]){
+        System.out.println("This is a Single player Snake & Ladder Game");
+        int pos;
+        
+    }
+}
